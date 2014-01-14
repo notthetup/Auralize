36b5b972026eb4f3414723d7a6f55d3218bf4450
Chinmay Pendharkar <notthetup@gmail.com>
2014-01-14 12:48:15 +0800

    testing routes

--- Auralize/CIViewController.m
+++ Auralize/CIViewController.m
@@ -18,12 +18,69 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self initAudioProcessing];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)switchValueChanged:(id)sender {
+    UISwitch *thisSwitch = (UISwitch *) sender;
+    
+    if ([thisSwitch isOn])
+        [self playAudioChain];
+    else
+        [self pauseAudioChain];
+}
+
+
+- (void) playAudioChain{
+    
+}
+
+- (void) pauseAudioChain{
+    
+}
+
+- (void) initAudioProcessing{
+    
+    NSError *err = nil;
+    if (![[AVAudioSession sharedInstance] setActive:YES error:&err]){
+        NSLog(@"Couldn't activate audio session: %@", err);
+    }
+    
+    
+    NSLog(@"Audio Inputs Avilable: ");
+    int counter = 0;
+    for (AVAudioSessionPortDescription *input in [[AVAudioSession sharedInstance] availableInputs]) {
+        NSLog(@"#%d Name :%@ - Type :%@ - Num Channels :%lu", counter++, input.portName, input.portType, (unsigned long)[input.channels count]);
+        for (AVAudioSessionChannelDescription *channel in input.channels){
+            NSLog(@"--#%lu Name :%@ - Label :%u", (unsigned long)channel.channelNumber, channel.channelName, (unsigned int)channel.channelLabel);
+        }
+    }
+    
+    [self setupMicrophoneInput];
+    [self setupOutput];
+    [self hookUpAudioChain];
+}
+
+- (void) setupMicrophoneInput{
+    //kAudioSessionMode_Measurement
+
+}
+
+- (void) setupOutput{
+    
+}
+
+- (void) hookUpAudioChain{
+    
+    
 }
 
 @end
